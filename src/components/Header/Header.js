dc309c3c47083c8089674e74801b5e6f03d88cb9
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-30 11:11:38 -1000

    Header component WIP

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="header">
+        <div id="header_logo">CMS</div>
+        <a id="login_button">
+          <Link to="/login">Login</Link>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Header;
