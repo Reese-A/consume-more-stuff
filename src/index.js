ed9247238631d34835eb9636bd26f4e3a68a9efc
cryosx <cyruswu.email@gmail.com>
2018-05-29 18:12:49 -1000

    Set index file up with Redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from './containers/App/App';
+import reducers from './redux/reducers/_reducers';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const boundCompose = compose.bind(null, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? boundCompose(window.__REDUX_DEVTOOLS_EXTENSION__())
+    : boundCompose()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
+
+export default store;
