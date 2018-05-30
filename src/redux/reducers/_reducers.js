9a2273a622e9bc53be1cb95b494bb99a99117e87
cryosx <cyruswu.email@gmail.com>
2018-05-29 18:13:13 -1000

    Renamed reducer file and combined all reducers

---
+++ src/redux/reducers/_reducers.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import category from './catergory-reducer';
+import condition from './condition-reducer';
+import item from './item-reducer';
+import status from './status-reducer';
+import user from './user-reducer';
+
+export default combineReducers({ category, condition, item, status, user });
