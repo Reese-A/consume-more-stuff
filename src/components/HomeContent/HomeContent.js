a3158c7e07e53e943cbec99fb103391f7a54666c
cryosx <cyruswu.email@gmail.com>
2018-05-30 10:49:17 -1000

    Boilerplate code for HomeContent.js

---
+++ src/components/HomeContent/HomeContent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class HomeContent extends React.Component {
+  constructor() {}
+  render() {
+    return <div>Home Content</div>;
+  }
+}
+
+export default HomeContent;
