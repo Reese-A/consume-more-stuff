07e3bfe1ac1b321605bdbd0f19d8641c8fa93d5a
cryosx <cyruswu.email@gmail.com>
2018-05-30 10:48:56 -1000

    Added HomeContent component to App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HomeContent from '../../components/HomeContent';
+
 class App extends Component {
   render() {
-    return <div> Consume More Stuff </div>;
+    return (
+      <div>
+        Consume More Stuff
+        <HomeContent />
+      </div>
+    );
   }
 }
 
