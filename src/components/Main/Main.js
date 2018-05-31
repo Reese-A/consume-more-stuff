e3868bfa0841f2780954a4bf53d786f3baa1370e
cryosx <cyruswu.email@gmail.com>
2018-05-30 16:16:46 -1000

    Added route handling for categories

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../Home/Home';
+import Category from '../Category/Category';
 
 const Main = props => {
   return (
     <main className="page_content">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/category/:name" component={Category} />
       </Switch>
     </main>
   );
