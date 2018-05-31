97b68bad073be0a0eb3e3401e0fbbf68979e88a8
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-31 11:23:21 -1000

    Added login route

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../Home/Home';
 import Category from '../Category/Category';
+import Login from '../../containers/Login/Login';
 
 const Main = props => {
   return (
@@ -10,6 +11,7 @@ const Main = props => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/category/:name" component={Category} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </main>
   );
