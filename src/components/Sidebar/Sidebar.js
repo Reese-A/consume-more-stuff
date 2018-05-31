8ad9b4e0945c15dafe82b20fe0f43414adc12e72
cryosx <cyruswu.email@gmail.com>
2018-05-30 14:54:17 -1000

    Added CSS to Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 
+import './Sidebar.css';
+
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
