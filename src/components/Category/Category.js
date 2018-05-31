7384f5b4b829c6f55a478f8c63bc516e8bee864a
cryosx <cyruswu.email@gmail.com>
2018-05-31 08:56:01 -1000

    Styled the Category component

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -2,17 +2,40 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-import Row from '../Row/Row';
+import Card from '../Card/Card';
+
+import './Category.css';
 
 class Category extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    console.log(this.props.match.params.name);
+    const { name } = this.props.match.params;
+    const category = this.props.categories.find(category => {
+      return category.name === name;
+    });
+    const id = category ? category.id : undefined;
+
+    let items = this.props.items[id] || [];
+
+    console.log(items);
     return (
       <div id="category">
-        <h1>{this.props.match.params.name}</h1>
+        <span className="category_title">{this.props.match.params.name}</span>
+        <div className="category_container">
+          {items.map(item => {
+            return (
+              <Card
+                key={item.id}
+                description={item.description}
+                price={item.price}
+                img_url={'http://via.placeholder.com/250x200'}
+                created_at={item.created_at}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
