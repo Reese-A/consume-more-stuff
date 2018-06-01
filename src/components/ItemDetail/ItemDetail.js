828b105b83d86c859ffbcc5471307704bd1be8a1
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-31 14:53:27 -1000

    Created ItemDetail component, WIP

---
+++ src/components/ItemDetail/ItemDetail.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadItem } from '../../redux/actions/item-actions';
+
+class ItemDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.loadItem(id);
+  }
+
+  render() {
+    console.log(this.props.item);
+    return <div>Hello World</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    item: state.item
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadItem: id => {
+      dispatch(loadItem(id));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemDetail);
