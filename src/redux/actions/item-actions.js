eccf2ca7d5d0eb1193acf868bf655b3ff244df32
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-31 14:54:29 -1000

    Created loadItem action

--- src/redux/actions/item-actions.js
+++ src/redux/actions/item-actions.js
@@ -1,4 +1,5 @@
 export const LOAD_ITEMS = 'LOAD_ITEMS';
+export const LOAD_ITEM = 'LOAD_ITEM';
 
 export const loadItems = () => {
   return dispatch => {
@@ -8,6 +9,19 @@ export const loadItems = () => {
         return dispatch({
           type: LOAD_ITEMS,
           items
+        });
+      });
+  };
+};
+
+export const loadItem = id => {
+  return dispatch => {
+    return fetch(`/item/${id}`)
+      .then(res => res.json())
+      .then(item => {
+        return dispatch({
+          type: LOAD_ITEM,
+          item
         });
       });
   };
