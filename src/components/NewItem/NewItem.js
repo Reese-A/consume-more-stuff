6880a9a3881b96cbfeb9d5fa1f79426ca4b92696
cryosx <cyruswu.email@gmail.com>
2018-06-01 11:23:28 -1000

    Removed react-form

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, NavLink, withRouter } from 'react-router-dom';
-import {
-  Form,
-  Text,
-  TextArea,
-  Radio,
-  RadioGroup,
-  Select,
-  Checkbox
-} from 'react-form';
 
 import { newItem } from '../../redux/actions/item-actions';
 
@@ -26,55 +17,7 @@ class NewItem extends React.Component {
   }
 
   render() {
-    const categories = this.props.categories || [];
-    const conditions = this.props.conditions || [];
-
-    const categoryOptions = categories.map(category => {
-      return {
-        label: category.name,
-        value: category.id
-      };
-    });
-
-    const conditionOptions = conditions.map(condition => {
-      return {
-        label: condition.name,
-        value: condition.id
-      };
-    });
-    return (
-      <div id="new_item" className="new_item">
-        <Form onSubmit={this.handleSubmit}>
-          {formApi => {
-            console.log(formApi);
-            return (
-              <form onSubmit={formApi.submitForm} id="new_item_form">
-                <label htmlFor="description">Description</label>
-                <Text required field="description" id="description" />
-                <Select
-                  required
-                  field="category"
-                  id="category"
-                  options={categoryOptions}
-                  className=""
-                />
-                <Select
-                  required
-                  field="condition"
-                  id="condition"
-                  options={conditionOptions}
-                  className=""
-                />
-
-                <button type="submit" className="">
-                  Submit
-                </button>
-              </form>
-            );
-          }}
-        </Form>{' '}
-      </div>
-    );
+    return <div>New Item</div>;
   }
 }
 
