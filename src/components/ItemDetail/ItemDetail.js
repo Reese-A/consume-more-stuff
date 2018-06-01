0e86ea5d5d98a8467c6de1542c3e9be2c977a129
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-31 16:02:00 -1000

    Built render for ItemDetail component

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loadItem } from '../../redux/actions/item-actions';
+import { Link } from 'react-router-dom';
 
 class ItemDetail extends Component {
   constructor(props) {
@@ -10,11 +11,37 @@ class ItemDetail extends Component {
   componentDidMount() {
     const id = this.props.match.params.id;
     this.props.loadItem(id);
+    console.log('MOUNT', this.props.item);
   }
 
   render() {
-    console.log(this.props.item);
-    return <div>Hello World!</div>;
+    const category = this.props.item.category
+      ? this.props.item.category.name
+      : null;
+    const condition = this.props.item.condition
+      ? this.props.item.condition.name
+      : null;
+    const owner = this.props.item.owner ? this.props.item.owner.name : null;
+    return (
+      <div id="item_detail">
+        <div>{this.props.item.description}</div>
+        <div>{this.props.item.price}</div>
+        <img src={this.props.item.img_url} alt="" />
+        <ul id="details_box">
+          Item Details:
+          <li>{condition}</li>
+          <li>{this.props.make}</li>
+          <li>{this.props.model}</li>
+          <li>{this.props.dimensions}</li>
+        </ul>
+        <div>{this.props.item.notes}</div>
+        <div>
+          Posted on {this.props.item.created_at} in{' '}
+          <Link to={`/category/${category}`}>{category}</Link> by {owner}
+        </div>
+        <div>Updated at {this.props.item.updated_at}</div>
+      </div>
+    );
   }
 }
 
