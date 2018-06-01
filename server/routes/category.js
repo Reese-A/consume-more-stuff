8ea2b1ff7ca9f7ef725f88d23900a751b55b37e9
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-06-01 13:41:05 -1000

    Created route to get items by category id

--- server/routes/category.js
+++ server/routes/category.js
@@ -5,39 +5,56 @@ const Item = require('../db/models/Item');
 const router = express.Router();
 
 router.route('/').get((req, res) => {
-  return Category.fetchAll({}).then(categories => {
-    return res.json(categories);
-  });
+  return Category.fetchAll({})
+    .then(categories => {
+      return res.json(categories);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 router.route('/items').get((req, res) => {
   console.log(req.query.limit);
   const limit = req.query.limit;
-  return Category.fetchAll().then(categories => {
-    const promises = categories.map(category => {
-      return new Category({ id: category.id }).fetch({
-        withRelated: [
-          {
-            items: qb => {
-              qb.limit(limit);
+  return Category.fetchAll()
+    .then(categories => {
+      const promises = categories.map(category => {
+        return new Category({
+          id: category.id
+        }).fetch({
+          withRelated: [
+            {
+              items: qb => {
+                qb.limit(limit);
+              }
             }
-          }
-        ]
+          ]
+        });
       });
-    });
 
-    Promise.all(promises).then(data => {
-      return res.json(data);
+      Promise.all(promises).then(data => {
+        return res.json(data);
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
 });
 
-router.route('/:name').get((req, res) => {
-  const name = req.params.name;
-  return new Category({ name })
-    .fetch({ withRelated: 'items' })
-    .then(category => {
-      return res.json(category);
+router.route('/:id').get((req, res) => {
+  const id = req.params.id;
+  const page = req.query.page;
+  const limit = req.query.limit;
+  return new Item()
+    .where({ category_id: id })
+    .orderBy('created_at', 'desc')
+    .fetchPage({ pageSize: limit, page })
+    .then(items => {
+      return res.json(items);
+    })
+    .catch(err => {
+      console.log(err);
     });
 });
 
