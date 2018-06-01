133c74e67a71dc979edb621d414d114f4e6678cf
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-31 23:20:49 -1000

    Added styling for ItemDetail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { loadItem } from '../../redux/actions/item-actions';
 import { Link } from 'react-router-dom';
 
+import './ItemDetail.css';
+
 class ItemDetail extends Component {
   constructor(props) {
     super(props);
@@ -21,25 +23,65 @@ class ItemDetail extends Component {
     const condition = this.props.item.condition
       ? this.props.item.condition.name
       : null;
-    const owner = this.props.item.owner ? this.props.item.owner.name : null;
     return (
       <div id="item_detail">
-        <div>{this.props.item.description}</div>
-        <div>{this.props.item.price}</div>
-        <img src={this.props.item.img_url} alt="" />
-        <ul id="details_box">
-          Item Details:
-          <li>{condition}</li>
-          <li>{this.props.make}</li>
-          <li>{this.props.model}</li>
-          <li>{this.props.dimensions}</li>
-        </ul>
-        <div>{this.props.item.notes}</div>
-        <div>
-          Posted on {this.props.item.created_at} in{' '}
-          <Link to={`/category/${category}`}>{category}</Link> by {owner}
+        <div id="item_main">
+          <span id="item_description">{this.props.item.description}</span>
+          <span id="item_price">{this.props.item.price}</span>
+          <img id="item_img" src={this.props.item.img_url} alt="" />
         </div>
-        <div>Updated at {this.props.item.updated_at}</div>
+        <div id="details_wrap">
+          <ul id="details_box">
+            <div id="details_header">Item Details</div>
+            <li id="item_condition">Condition: {condition}</li>
+            <li>make</li>
+            <li>model</li>
+            <li>dimensions</li>
+            {this.props.make ? (
+              <li id="item_make">Make: {this.props.make}</li>
+            ) : null}
+            {this.props.model ? (
+              <li id="item_model">Model: {this.props.model}</li>
+            ) : null}
+            {this.props.dimensions ? (
+              <li id="item_dimensions">Dimensions: {this.props.dimensions}</li>
+            ) : null}
+          </ul>
+          <div id="item_notes">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nam
+            illo excepturi quos quisquam sit in quod voluptatum necessitatibus,
+            sequi, cumque dolores ipsa dolorem. Eligendi, ut! Nihil sequi natus
+            maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Maxime nam illo excepturi quos quisquam sit in quod voluptatum
+            necessitatibus, sequi, cumque dolores ipsa dolorem. Eligendi, ut!
+            Nihil sequi natus maiores. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Maxime nam illo excepturi quos quisquam sit in
+            quod voluptatum necessitatibus, sequi, cumque dolores ipsa dolorem.
+            Eligendi, ut! Nihil sequi natus maiores. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Maxime nam illo excepturi quos
+            quisquam sit in quod voluptatum necessitatibus, sequi, cumque
+            dolores ipsa dolorem. Eligendi, ut! Nihil sequi natus maiores.Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Maxime nam illo
+            excepturi quos quisquam sit in quod voluptatum necessitatibus,
+            sequi, cumque dolores ipsa dolorem. Eligendi, ut! Nihil sequi natus
+            maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Maxime nam illo excepturi quos quisquam sit in quod voluptatum
+            necessitatibus, sequi,cumque dolores ipsa dolorem. Eligendi, ut!
+            Nihil sequi natus maiores. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Maxime nam illo excepturi quos quisquam sit in
+            quod voluptatum necessitatibus, sequi,cumque dolores ipsa dolorem.
+            Eligendi, ut! Nihil sequi natus maiores.{this.props.item.notes}
+          </div>
+        </div>
+        <div id="item_footer">
+          <div id="item_created_at">
+            Posted on {this.props.item.created_at} in{' '}
+            <Link to={`/category/${category}`}>{category}</Link>
+          </div>
+          <div id="item_updated_at">
+            Updated at {this.props.item.updated_at}
+          </div>
+        </div>
       </div>
     );
   }
