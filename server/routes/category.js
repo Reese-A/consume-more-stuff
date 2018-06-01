458d3b445ef482f35bc6b34fdc13887ab0c8a4cb
cryosx <cyruswu.email@gmail.com>
2018-06-01 13:03:29 -1000

    Added route to get items per category with limit

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,11 +1,34 @@
 const express = require('express');
 
 const Category = require('../db/models/Category');
+const Item = require('../db/models/Item');
 const router = express.Router();
 
 router.route('/').get((req, res) => {
   return Category.fetchAll({}).then(categories => {
     return res.json(categories);
+  });
+});
+
+router.route('/items').get((req, res) => {
+  console.log(req.query.limit);
+  const limit = req.query.limit;
+  return Category.fetchAll().then(categories => {
+    const promises = categories.map(category => {
+      return new Category({ id: category.id }).fetch({
+        withRelated: [
+          {
+            items: qb => {
+              qb.limit(limit);
+            }
+          }
+        ]
+      });
+    });
+
+    Promise.all(promises).then(data => {
+      return res.json(data);
+    });
   });
 });
 
