176c18c2eec1d0a296a76f3c4aea3587e8afc7d7
cryosx <cyruswu.email@gmail.com>
2018-06-01 14:11:24 -1000

    Added loadCategoryItems action

--- src/redux/actions/category-actions.js
+++ src/redux/actions/category-actions.js
@@ -1,4 +1,5 @@
 export const LOAD_CATEGORIES = 'LOAD_CATEGORIES';
+export const LOAD_CATEGORY_ITEMS = 'LOAD_CATEGORY_ITEMS';
 
 export const loadCategories = () => {
   return dispatch => {
@@ -7,6 +8,19 @@ export const loadCategories = () => {
       .then(categories => {
         return dispatch({
           type: LOAD_CATEGORIES,
+          categories
+        });
+      });
+  };
+};
+
+export const loadCategoryItems = () => {
+  return dispatch => {
+    return fetch('/category/items?limit=5')
+      .then(res => res.json())
+      .then(categories => {
+        return dispatch({
+          type: LOAD_CATEGORY_ITEMS,
           categories
         });
       });
