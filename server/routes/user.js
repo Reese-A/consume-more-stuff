afb574d1375bc2647823bdfad608bc78e334ba11
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-06-02 11:45:19 -1000

    Added user login logout and register routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 const User = require('../db/models/User');
 const router = express.Router();
+const saltedRounds = 12;
 
 router.route('/').get((req, res) => {
   return User.fetchAll({})
@@ -11,6 +14,47 @@ router.route('/').get((req, res) => {
     .catch(err => {
       console.log(err);
     });
+});
+
+router.route('/register').post((req, res) => {
+  let { email, password, name } = req.body;
+
+  email = email.trim();
+  name = name.trim();
+
+  bcrypt.genSalt(saltedRounds, function(err, salt) {
+    if (err) {
+      console.log(err);
+    }
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      if (err) {
+        console.log(err);
+      }
+      return new User({
+        email,
+        name,
+        password: hash
+      })
+        .save()
+        .then(user => {
+          return res.json(user);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  });
+});
+
+router.route('/login').post(passport.authenticate('local'), (req, res) => {
+  return res.json(req.user);
+});
+
+router.route('/logout').get((req, res) => {
+  req.logout();
+  return res.json({
+    success: true
+  });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const Redis = require('connect-redis')(session);
+
+const User = require('./db/models/User');
 
 const server = express();
 const PORT = process.env.PORT || 9001;
@@ -11,6 +18,86 @@ server.use(
   })
 );
 server.use(bodyParser.json());
+
+server.use(
+  session({
+    store: new Redis(),
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+server.use(passport.initialize());
+server.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  console.log('serializing');
+  return done(null, {
+    id: user.id,
+    email: user.email
+  });
+});
+
+passport.deserializeUser((user, done) => {
+  console.log('deserializing');
+  new User({
+    id: user.id
+  })
+    .fetch()
+    .then(user => {
+      user = user.toJSON();
+      return done(null, {
+        id: user.id,
+        name: user.name
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return done(err);
+    });
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    function(email, password, done) {
+      return new User({
+        email: email
+      })
+        .fetch()
+        .then(user => {
+          user = user.toJSON();
+          console.log(user);
+
+          if (user === null) {
+            return done(null, false, {
+              message: 'bad email or password'
+            });
+          } else {
+            console.log(password, user.password);
+            bcrypt.compare(password, user.password).then(res => {
+              if (res) {
+                console.log(res);
+                return done(null, user);
+              } else {
+                return done(null, false, {
+                  message: 'Bad email or password'
+                });
+              }
+            });
+          }
+        })
+        .catch(err => {
+          console.log('error', err);
+          return done(err);
+        });
+    }
+  )
+);
 
 server.use('/api', routes);
 
