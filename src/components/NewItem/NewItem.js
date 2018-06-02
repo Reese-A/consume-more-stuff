be7311fbb7c47eb9e7f4c2ff8a3a22b002cd7e9c
cryosx <cyruswu.email@gmail.com>
2018-06-02 05:43:21 -1000

    Added more fields in

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -33,16 +33,26 @@ class NewItem extends React.Component {
       );
     });
     return (
-      <div className="new_item">
+      <div id="new_item">
+        <span id="new_item_title">Add An Item</span>
         <form>
-          <input type="text" />
-          <input type="text" />
-          <select name="category" id="new_item_category">
+          <input type="text" name="description" required />
+          <input type="number" name="price" />
+          <input type="text" name="units" />
+
+          <input type="text" name="make" />
+          <input type="text" name="model" />
+          <input type="text" name="dimensions" />
+          <textarea type="text" name="notes" />
+          <input type="file" name="img" />
+
+          <select name="category" id="new_item_category" required>
             {categoryOptions}
           </select>
-          <select name="condition" id="new_item_condition">
+          <select name="condition" id="new_item_condition" required>
             {conditionOptions}
           </select>
+          <button type="submit">Add Item</button>
         </form>
       </div>
     );
