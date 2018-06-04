e6a8ddc2dfa64a21a0a1b7ff1da6c320135d0f86
cryosx <cyruswu.email@gmail.com>
2018-06-04 09:05:29 -1000

    Worked on AWS integration

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const AWS = require('aws-sdk');
+require('dotenv').config();
 
 const Item = require('../db/models/Item');
 const router = express.Router();
+
+// const credentials = new AWS.CognitoIdentityCredentials({
+//   IdentityPoolId: 'IDENTITY_POOL_ID'
+// });
+// const config = new AWS.Config({
+//   credentials: myCredentials,
+//   region: 'us-west-1'
+// });
+
+// const s3 = new AWS.S3(config);
+
+const Bucket = process.env.AWS_S3_BUCKET_NAME;
+const region = process.env.AWS_S3_BUCKET_REGION;
+const IdentityPoolId = process.env.AWS_IDENTITY_POOL_ID;
+const credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId
+});
+
+AWS.config.update({
+  region,
+  credentials
+});
+
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: { Bucket }
+});
 
 router
   .route('/')
