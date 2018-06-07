9d61396462c6c99e19aa52e5f8f0c40ec8dbadab
cryosx <cyruswu.email@gmail.com>
2018-06-07 10:38:43 -1000

    Test route for sendgrid

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,6 +7,19 @@ const isAuthenticated = require('../utilities/isAuthenticated');
 const router = express.Router();
 const saltedRounds = 12;
 
+router.route('/email-test').get((req, res) => {
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'cyruswu.email@gmail.com',
+    from: 'cms@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+  };
+  sgMail.send(msg);
+});
+
 router.route('/:id/items').get(isAuthenticated, (req, res) => {
   const { id } = req.params;
   const page = req.query.page > 0 ? req.query.page : 1;
